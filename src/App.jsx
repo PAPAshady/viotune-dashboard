df2ef65703133f49e2b1fc9c6dffa234cda9122d
Nima Zamani <zamani.nima18@gmail.com>
2025-12-17 21:01:11 +0330

    feat: implemented the basic layout of the page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { SidebarProvider } from './components/ui/sidebar';
+
+import Header from './components/shared/Header/Header';
+import AppSidebar from './components/shared/AppSidebar/AppSidebar';
+import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="grow">
+        <Header />
+      </main>
+    </SidebarProvider>
+  );
 }
 
 export default App;
---
+++ src/components/shared/Header/Header.jsx
@@ -0,0 +1,33 @@
+import { SearchIcon, MenuIcon } from 'lucide-react';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { InputGroup, InputGroupInput, InputGroupAddon } from '@/components/ui/input-group';
+import { useSidebar } from '@/components/ui/sidebar';
+
+import defaultAvatar from '@assets/images/default-avatar.png';
+
+function Header() {
+  const { toggleSidebar } = useSidebar();
+  return (
+    <div className="border-secondary xs:px-6 xs:gap-3 bg-background sticky top-0 flex w-full items-center justify-between gap-2 border-b p-3.25">
+      <button className="md:hidden">
+        <MenuIcon onClick={toggleSidebar} size={20} />
+      </button>
+
+      <InputGroup className="md:max-w-md">
+        <InputGroupInput placeholder="Search..." />
+        <InputGroupAddon>
+          <SearchIcon />
+        </InputGroupAddon>
+      </InputGroup>
+      <Avatar className="md:size-10">
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>
+          <img src={defaultAvatar} alt="Avatar" />
+        </AvatarFallback>
+      </Avatar>
+    </div>
+  );
+}
+
+export default Header;
