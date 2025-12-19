d0a40f91ea9674622bdc75313dfc646311af8506
Nima Zamani <zamani.nima18@gmail.com>
2025-12-19 14:57:19 +0330

    feat: implemented time range select box for UsersChart and session duration chart.

--- src/components/Charts/SessionDurationChart/SessionDurationChart.jsx
+++ src/components/Charts/SessionDurationChart/SessionDurationChart.jsx
@@ -1,5 +1,23 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
-import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+import { useState } from 'react';
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardAction,
+} from '@components/ui/card';
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@components/ui/chart';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectItem,
+} from '@components/ui/select';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 import useMediaQuery from '@/hooks/useMediaQuery';
@@ -22,13 +40,35 @@ const chartConfig = {
 };
 
 function SessionDurationChart() {
+  const [range, setRange] = useState('last-3-months');
   const isTablet = useMediaQuery('(min-width: 768px)');
+
+  const handleChange = (value) => {
+    if (!value) return;
+    setRange(value);
+    console.log(value);
+  };
 
   return (
     <Card className="grow">
-      <CardHeader>
+      <CardHeader className="px-3 sm:px-6">
         <CardTitle>Average Session Duration</CardTitle>
         <CardDescription>Track average session duration.</CardDescription>
+        <CardAction>
+          <Select onValueChange={handleChange} value={range}>
+            <SelectTrigger>
+              <SelectValue placeholder="Last 3 months" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Select a time range</SelectLabel>
+                <SelectItem value="last-3-months">Last 3 months</SelectItem>
+                <SelectItem value="last-30-days">Last 30 days</SelectItem>
+                <SelectItem value="last-7-days">Last 7 days</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </CardAction>
       </CardHeader>
       <CardContent className="px-2 md:ps-0 md:pe-6">
         <ChartContainer config={chartConfig} className="max-h-50 min-h-42 w-full">
