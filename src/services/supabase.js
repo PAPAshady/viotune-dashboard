b17660609e51507235bbf5d22ba7573694687495
Nima Zamani <zamani.nima18@gmail.com>
2025-12-27 20:47:29 +0330

    feat: added a comment in supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://okuypoeobjphvsgwjrce.supabase.co';
+
+// this is supabase public key and is safe to use in the browser.
 const publicKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdXlwb2VvYmpwaHZzZ3dqcmNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NTM3MjIsImV4cCI6MjA1NzAyOTcyMn0.zqBBnRDaa6gIS6ovk6x2GUwFLITepC1RA6gU7r9boRg';
 
