ac635771677176d2aac572f5f87fb10382d1476e
Nima Zamani <zamani.nima18@gmail.com>
2025-12-31 21:01:34 +0330

    feat: used .env file to store supabase credentials.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://okuypoeobjphvsgwjrce.supabase.co';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabasePublicKey = import.meta.env.VITE_SUPABASE_KEY;
 
-// this is supabase public key and is safe to use in the browser.
-const publicKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdXlwb2VvYmpwaHZzZ3dqcmNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NTM3MjIsImV4cCI6MjA1NzAyOTcyMn0.zqBBnRDaa6gIS6ovk6x2GUwFLITepC1RA6gU7r9boRg';
-
-const supabase = createClient(supabaseUrl, publicKey);
+const supabase = createClient(supabaseUrl, supabasePublicKey);
 
 export default supabase;
