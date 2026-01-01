3efcfee071edbe83488d5527a57be8d4257e86f9
Nima Zamani <zamani.nima18@gmail.com>
2026-01-01 13:41:52 +0330

    feat: creator column of playlists tabel now shows different roles with different colors.

--- src/components/Tables/ColumnDefs/Cells/PlaylistsTableCreatorCell.jsx
+++ src/components/Tables/ColumnDefs/Cells/PlaylistsTableCreatorCell.jsx
@@ -1,27 +1,33 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar';
 import { Badge } from '@components/ui/badge';
+import { cn } from '@/lib/utils';
 
 import defaultAvatar from '@assets/images/default-avatar.png';
 
 function PlaylistsTableCreatorCell({ row }) {
-  const hasCreator = row.original.creator;
+  const creator = row.original.creator;
 
-  if(!hasCreator) return <span className='text-muted-foreground'>Unknown creator</span>;
+  if (!creator) return <span className="text-muted-foreground">Unknown creator</span>;
 
   return (
     <div className="flex items-center gap-2">
       <Avatar className="md:size-10">
-        <AvatarImage src={row.original.creator?.avatar_url} alt="Avatar" />
+        <AvatarImage src={creator.avatar_url} alt="Avatar" />
         <AvatarFallback>
           <img src={defaultAvatar} className="size-full object-cover" />
         </AvatarFallback>
       </Avatar>
       <div className="space-y-1">
         <div className="flex items-center gap-2">
-          <span>{row.original.creator?.full_name}</span>
-          <Badge variant="secondary">{row.original.creator?.role.replace('_', ' ')}</Badge>
+          <span>{creator.full_name}</span>
+          <Badge
+            className={cn('capitalize', creator.role === 'super_admin' && 'bg-blue-500 text-white')}
+            variant={creator.role === 'admin' ? 'default' : 'secondary'}
+          >
+            {creator.role.replace('_', ' ')}
+          </Badge>
         </div>
-        <p className="text-muted-foreground text-xs">{row.original.creator?.email}</p>
+        <p className="text-muted-foreground text-xs">{creator.email}</p>
       </div>
     </div>
   );
