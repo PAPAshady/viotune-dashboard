fa3f2380cad6056ea41fc9b5f59b69b493ab1efe
Nima Zamani <zamani.nima18@gmail.com>
2026-01-03 21:21:48 +0330

    fix: updated getMe function because backend updated.

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,7 @@
 import supabase from './supabase';
 
 export const getMe = async (userId) => {
-  const { data, error } = await supabase.from('users').select('*').eq('auth_id', userId).single();
+  const { data, error } = await supabase.from('users').select('*').eq('id', userId).single();
   if (error) throw error;
   return data;
 };
