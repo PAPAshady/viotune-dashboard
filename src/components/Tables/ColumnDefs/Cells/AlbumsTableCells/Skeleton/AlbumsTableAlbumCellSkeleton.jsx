562f971b89fbad34f9af29552990d48bdcc35aa1
Nima Zamani <zamani.nima18@gmail.com>
2026-01-04 20:40:12 +0330

    feat: implemented loading state for albums tabel.

---
+++ src/components/Tables/ColumnDefs/Cells/AlbumsTableCells/Skeleton/AlbumsTableAlbumCellSkeleton.jsx
@@ -0,0 +1,12 @@
+import { Skeleton } from '@components/ui/skeleton';
+
+function AlbumsTableAlbumCellSkeleton() {
+  return (
+    <div className="flex items-center gap-2">
+      <Skeleton className="size-12 min-w-12 rounded-md" />
+      <Skeleton className="h-2.5 w-10 max-w-30 grow lg:w-16" />
+    </div>
+  );
+}
+
+export default AlbumsTableAlbumCellSkeleton;
