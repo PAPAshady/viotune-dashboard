fe98dfa42fc2963404473f492bd4988fe49fa754
Nima Zamani <zamani.nima18@gmail.com>
2026-01-22 17:14:24 +0330

    feat: implemented a logo component and used it in sidebar and global loading state.

--- src/components/shared/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/shared/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,25 @@
 import { Navigate } from 'react-router';
 
 import useAuth from '@/store/useAuth';
+import useMediaQuery from '@hooks/useMediaQuery';
+import Logo from '@components/Logo/Logo';
 
 function ProtectedRoute({ children }) {
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
   const user = useAuth((state) => state.user);
   const isLoading = useAuth((state) => state.isLoading);
 
-  if (isLoading) return 'Loading...';
+  if (isLoading)
+    return (
+      <div className="relative">
+        <div
+          className={`fixed inset-0 z-20 grid h-dvh w-full place-content-center backdrop-blur-md transition-all duration-300`}
+        >
+          <Logo size={isDesktop ? 'xl' : 'lg'} isLoading showText />
+        </div>
+        {children}
+      </div>
+    );
 
   if (!user) return <Navigate to="/auth/sign-in" />;
 
