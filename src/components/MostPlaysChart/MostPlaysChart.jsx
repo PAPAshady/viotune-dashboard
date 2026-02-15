f1155f21587847c205878879a2a3c2d97bd49ce7
Nima Zamani <zamani.nima18@gmail.com>
2026-02-15 22:10:54 +0330

    feat: implemented a loading state for mostPlasChart component.

--- src/components/MostPlaysChart/MostPlaysChart.jsx
+++ src/components/MostPlaysChart/MostPlaysChart.jsx
@@ -1,6 +1,7 @@
 import { Card, CardHeader, CardTitle, CardContent } from '@components/ui/card';
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@components/ui/chart';
 import { BarChart, XAxis, YAxis, Bar, CartesianGrid } from 'recharts';
+import { Spinner } from '@components/ui/spinner';
 
 import useMediaQuery from '@hooks/useMediaQuery';
 
@@ -9,7 +10,7 @@ const chartConfig = {
   color: 'var(--chart-3)',
 };
 
-function MostPlaysChart({ chartTitle, data, yAxisDataKey, barDataKey }) {
+function MostPlaysChart({ chartTitle, data, isPending, yAxisDataKey, barDataKey }) {
   const isMobile = useMediaQuery('(max-width: 640px)');
 
   return (
@@ -19,24 +20,30 @@ function MostPlaysChart({ chartTitle, data, yAxisDataKey, barDataKey }) {
       </CardHeader>
       <CardContent className="px-2 md:ps-0 md:pe-6">
         <ChartContainer config={chartConfig} className="max-h-75 w-full">
-          <BarChart data={data} layout="vertical">
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis type="number" />
-            <YAxis
-              dataKey={yAxisDataKey}
-              type="category"
-              tick={{ fontSize: isMobile ? 10 : 13 }}
-              width={isMobile ? 80 : 110}
-            />
-            <ChartTooltip content={<ChartTooltipContent />} />
-            <Bar
-              width={10}
-              dataKey={barDataKey}
-              name="Plays"
-              fill="oklch(62.3% 0.214 259.815)"
-              barSize={isMobile ? 15 : 25}
-            />
-          </BarChart>
+          {isPending ? (
+            <div className="flex size-full items-center justify-center">
+              <Spinner className="xs:size-13 size-10" />
+            </div>
+          ) : (
+            <BarChart data={data} layout="vertical">
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="number" />
+              <YAxis
+                dataKey={yAxisDataKey}
+                type="category"
+                tick={{ fontSize: isMobile ? 10 : 13 }}
+                width={isMobile ? 80 : 110}
+              />
+              <ChartTooltip content={<ChartTooltipContent />} />
+              <Bar
+                width={10}
+                dataKey={barDataKey}
+                name="Plays"
+                fill="oklch(62.3% 0.214 259.815)"
+                barSize={isMobile ? 15 : 25}
+              />
+            </BarChart>
+          )}
         </ChartContainer>
       </CardContent>
     </Card>
